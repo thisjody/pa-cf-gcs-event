719a3585a5b4c6008cb00e689af7d60b3354ad46
jody <thisjody@gmail.com>
2023-08-16 17:47:23 -0700

    first deploy

---
+++ eiedeploy.sh
@@ -0,0 +1,11 @@
+gcloud functions deploy pa-cf-gcs-event \
+  --gen2 \
+  --runtime=python311 \
+  --region=us-west1 \
+  --source=src \
+  --entry-point=gcs_event_to_pubsub \
+  --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
+  --trigger-event-filters="bucket=sw-pa-test" \
+  --memory 16384MB \
+  --timeout 540s  \
+  --set-env-vars PROJECT_ID=acep-ext-eielson-2021,TOPIC_NAME=pa-gcs-event
