4b4ba2227c0690986b34f9ce5bf64bc1863144f0
jody <thisjody@gmail.com>
2023-08-22 08:28:42 -0700

    inpersonation 1st version working

--- src/main.py
+++ src/main.py
@@ -1,10 +1,42 @@
-from google.cloud import pubsub_v1
+from google.cloud import pubsub_v1, secretmanager
+from google.oauth2 import service_account
+from google.auth.transport import requests
+from google.auth import impersonated_credentials
 import os
 import json
 import re
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(os.getenv('PROJECT_ID'), os.getenv('TOPIC_NAME'))
+# Setup the auth and request for token
+auth_request = requests.Request()
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+
+def get_secret(secret_name):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{PROJECT_ID}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(name=name)
+    return json.loads(response.payload.data.decode('UTF-8'))
+
+# Obtain service account credentials from Secret Manager
+sa_credentials = get_secret('pa-cf-deploy-sa-credentials')
+
+# Load the credentials for the less privileged SA from the secret
+credentials = service_account.Credentials.from_service_account_info(sa_credentials)
+
+target_principal = os.getenv('IMPERSONATE_SA')
+target_scopes = os.getenv('TARGET_SCOPES').split(",")
+
+# Build impersonated credentials for the target service account
+impersonated_creds = impersonated_credentials.Credentials(
+    source_credentials=credentials,
+    target_principal=target_principal,
+    target_scopes=target_scopes,
+    lifetime=600  # in seconds, optional
+)
+
+# Create a Pub/Sub publisher client using impersonated credentials
+publisher = pubsub_v1.PublisherClient(credentials=impersonated_creds)
+topic_path = publisher.topic_path(PROJECT_ID, os.getenv('TOPIC_NAME'))
 
 def gcs_event_to_pubsub(data, context):
     print(f"Event type: {context.event_type}")
@@ -12,7 +44,6 @@ def gcs_event_to_pubsub(data, context):
 
     resource_name = data['name'].rstrip('/')
 
-    #csv_pattern = r'^\d{4}/\d{2}/uaf-acep-[\w-]+/uaf-acep-[\w-]+_\d{4}-\d{2}-\d{2}\.csv$'
     csv_pattern = r'(?i)^\d{4}/\d{2}/uaf-acep-[\w-]+/uaf-acep-[\w-]+_\d{4}-\d{2}-\d{2}\.csv$'
 
     if re.match(csv_pattern, resource_name):
