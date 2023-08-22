2a4a95fdbf2858ce21da37f15e818cc7cf94b62b
jody <thisjody@gmail.com>
2023-08-22 08:27:03 -0700

    add changes for inpersonation

--- pa_deploy.sh
+++ pa_deploy.sh
@@ -2,11 +2,11 @@ gcloud functions deploy pa-cf-gcs-event \
   --gen2 \
   --runtime=python311 \
   --region=us-west1 \
-  --service-account=pa-cf-gcs-event-sa@acep-ext-eielson-2021.iam.gserviceaccount.com \
+  --service-account=pa-cf-deploy-sa@acep-ext-eielson-2021.iam.gserviceaccount.com \
   --source=src \
   --entry-point=gcs_event_to_pubsub \
   --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
   --trigger-event-filters="bucket=sw-pa-test" \
   --memory 16384MB \
   --timeout 540s  \
-  --set-env-vars PROJECT_ID=acep-ext-eielson-2021,TOPIC_NAME=pa-gcs-event
+  --set-env-vars PROJECT_ID=acep-ext-eielson-2021,TOPIC_NAME=pa-gcs-event,IMPERSONATE_SA=pa-gcs-ps-privileged-sa@acep-ext-eielson-2021.iam.gserviceaccount.com,TARGET_SCOPES="https://www.googleapis.com/auth/cloud-platform"
