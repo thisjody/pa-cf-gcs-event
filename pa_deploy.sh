a329b495e31bcbeac2c70ab993fd911b9c684afd
jody <thisjody@gmail.com>
2023-09-06 16:17:47 -0700

    updating sa for deploy and secret name

--- pa_deploy.sh
+++ pa_deploy.sh
@@ -8,4 +8,4 @@ gcloud functions deploy pa-cf-gcs-event \
   --trigger-event-filters="type=google.cloud.storage.object.v1.finalized,bucket=sw-pa-test" \
   --memory 16384MB \
   --timeout 540s \
-  --set-env-vars "PROJECT_ID=acep-ext-eielson-2021,TOPIC_NAME=pa-gcs-event,IMPERSONATE_SA=pa-gcs-ps-privileged-sa@acep-ext-eielson-2021.iam.gserviceaccount.com,TARGET_SCOPES=https://www.googleapis.com/auth/cloud-platform,SA_CREDENTIALS_SECRET_NAME=pa-cf-ea-deploy-sa-credentials"
+  --set-env-vars "PROJECT_ID=acep-ext-eielson-2021,TOPIC_NAME=pa-gcs-event,IMPERSONATE_SA=pa-gcs-ps-privileged-sa@acep-ext-eielson-2021.iam.gserviceaccount.com,TARGET_SCOPES=https://www.googleapis.com/auth/cloud-platform,SA_CREDENTIALS_SECRET_NAME=pa-cf-ea-deploy-gcs-event-sa-credentials"
